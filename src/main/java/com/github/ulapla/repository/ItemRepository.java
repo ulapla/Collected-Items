41bfd39530f2aaae51fb82c5a1edbaff9b93d9a6
Ula Placek <ulaplacek@gmail.com>
2019-07-14 15:45:21 +0200

    Repositories added

---
+++ src/main/java/com/github/ulapla/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.github.ulapla.repository;
+
+import com.github.ulapla.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
---
+++ src/main/java/com/github/ulapla/repository/LocationRepository.java
@@ -0,0 +1,7 @@
+package com.github.ulapla.repository;
+
+import com.github.ulapla.model.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+}
