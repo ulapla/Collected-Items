54f4cd8f768025a3cbcbaace218a935e2f2d8bee
Ula Placek <ulaplacek@gmail.com>
2019-07-22 18:57:13 +0200

    Spring security added

---
+++ src/main/java/com/github/ulapla/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.github.ulapla.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/admin/**").hasAnyRole("USER", "ADMIN")
+//                .anyRequest().permitAll()
+//                .and().formLogin()
+////                .loginPage("/login")
+//                .and().logout().logoutSuccessUrl("/")
+//                .permitAll();
+//    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin1").password("{noop}admin123").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .and().formLogin();
+    }
+//
+//    @Bean
+//    public SpringDataUserDetailsService customUserDetailsService() {
+//
+//        return new SpringDataUserDetailsService();
+//    }
+
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
